12afdfce4fd47675efd79cdbf086278519098acc
james_folk <jamesfolk1@gmail.com>
2016-02-08 16:47:01 -0500

    added a listener

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("It works!");
+var express = require('express');
+var app = express();
+
+app.set('port', 3000);
+
+var server = app.listen(app.get('port'), function()
+  {
+    var port = server.address().port;
+    console.log('Magic happens on port ' + port);
+  });
+console.log('Me first');
